3d5a646c79ef582e300f84d6ff2edd7d0cdcb173
minhle0105 <minh.anh.le.cnn@gmail.com>
2021-11-23 19:26:29 -0500

    EightPointSet

--- src/sorting/EightPointSets.java
+++ src/sorting/EightPointSets.java
@@ -4,18 +4,6 @@ import java.util.*;
 
 public class EightPointSets {
 
-    private static HashMap<Integer, Integer> countPoint(List<Integer> points) {
-        HashMap<Integer, Integer> hashMap = new HashMap<>();
-        for (Integer point : points) {
-            if (hashMap.containsKey(point)) {
-                hashMap.put(point, hashMap.get(point) + 1);
-            }
-            else {
-                hashMap.put(point, 1);
-            }
-        }
-        return hashMap;
-    }
 
     private static String solution(List<List<Integer>> points) {
         List<Integer> Xs = new ArrayList<>();
@@ -23,32 +11,50 @@ public class EightPointSets {
 
         // lấy tất cả toạ độ X và toạ độ Y
         for (List<Integer> point : points) {
-            Xs.add(point.get(0));
-            Ys.add(point.get(1));
+            if (!Xs.contains(point.get(0))) {
+                Xs.add(point.get(0));
+            }
+            if (!Ys.contains(point.get(1))) {
+                Ys.add(point.get(1));
+            }
         }
 
-        HashMap<Integer, Integer> countX = countPoint(Xs);
-        HashMap<Integer, Integer> countY = countPoint(Ys);
-
-        // nếu không có 3 toạ độ X hoặc 3 toạ độ Y phân biệt thì chắc chắn sai
-        if (countX.size() != 3 || countY.size() != 3) {
+        if (Xs.size() != 3 | Ys.size() != 3) {
             return "ugly";
         }
 
-        // sort toạ độ X và Y
-        List<Integer> uniqueX = new ArrayList<>(countX.keySet());
-        Collections.sort(uniqueX);
-        List<Integer> uniqueY = new ArrayList<>(countY.keySet());
-        Collections.sort(uniqueY);
+        Collections.sort(Xs);
+        Collections.sort(Ys);
 
-        // sort rồi thì điểm ở giữa chắc chắn có index 1 1
-        List<Integer> middlePoint = Arrays.asList(uniqueX.get(1), uniqueY.get(1));
+        List<List<Integer>> generatedPoints = new ArrayList<>();
+        for (Integer x : Xs) {
+            for (Integer y : Ys) {
+                if (Xs.indexOf(x) == 1 && Ys.indexOf(y) == 1) {
+                    continue;
+                }
+                else {
+                    List<Integer> point = new ArrayList<>();
+                    point.add(x);
+                    point.add(y);
+                    generatedPoints.add(point);
+                }
+            }
+        }
 
-        // nếu trong list các điểm ban đầu có điểm ở giữa thì sai
-        if (points.contains(middlePoint)) {
-            return "ugly";
+        Collections.sort(points, (o1, o2) -> {
+            if (!o1.get(0).equals(o2.get(0))) {
+                return o1.get(0).compareTo(o2.get(0));
+            }
+            return o1.get(1).compareTo(o2.get(1));
+        });
+
+        for (int i = 0; i < points.size(); i++) {
+            if (!points.get(i).get(0).equals(generatedPoints.get(i).get(0)) | !points.get(i).get(1).equals(generatedPoints.get(i).get(1))) {
+                return "ugly";
+            }
         }
         return "respectable";
+
     }
 
 
