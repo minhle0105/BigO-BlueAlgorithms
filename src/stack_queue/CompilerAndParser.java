d7bbd637610834727e4074c6d1e8627b9972db2b
minhle0105 <minh.anh.le.cnn@gmail.com>
2021-11-27 13:21:04 -0500

    Compiler and Parser time limit exceeded

--- src/stack_queue/CompilerAndParser.java
+++ src/stack_queue/CompilerAndParser.java
@@ -3,29 +3,47 @@ package stack_queue;
 import java.util.*;
 
 public class CompilerAndParser {
+    private int length;
 
-    private static int solution(String s) {
-        if (s.toCharArray()[0] == '>') {
-            return 0;
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    private void validR(String s) {
+        if (s.length() <= 1) {
+            this.length = 0;
+            return;
         }
-        int result = 0;
         Stack<Character> stack = new Stack<>();
         for (char c : s.toCharArray()) {
             if (c == '<') {
                 stack.push(c);
             }
-            if ((!stack.isEmpty()) && (c == '>' && stack.peek() == '<')) {
+            else if ((!stack.isEmpty()) && (c == '>' && stack.peek() == '<')) {
                 stack.pop();
-                result += 2;
+            }
+            else {
+                stack.push(c);
             }
         }
-        if (!stack.isEmpty()) {
-            return 0;
+        if (stack.isEmpty()) {
+            length = s.length();
         }
-        return result;
+        else {
+            validR(s.substring(0, s.length() - 1));
+        }
     }
 
+    private int solution(String s) {
+        if (s.toCharArray()[0] == '>') {
+            return 0;
+        }
+        validR(s);
+        return length;
+    }// <<><<>><<
+
     public static void main(String[] args) {
+        CompilerAndParser compilerAndParser = new CompilerAndParser();
         Scanner sc = new Scanner(System.in);
         int n = Integer.parseInt(sc.next());
         List<String> list = new ArrayList<>();
@@ -33,7 +51,8 @@ public class CompilerAndParser {
             list.add(sc.next());
         }
         for (String s : list) {
-            System.out.println(solution(s));
+            compilerAndParser.setLength(0);
+            System.out.println(compilerAndParser.solution(s));
         }
         sc.close();
     }
