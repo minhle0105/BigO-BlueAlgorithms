071434a25dc4351861d61489e3004bcb227dfced
minhle0105 <minh.anh.le.cnn@gmail.com>
2021-11-30 13:07:57 -0500

    Try using linked list

--- src/stack_queue/YourQueue.java
+++ src/stack_queue/YourQueue.java
@@ -4,33 +4,27 @@ import java.util.*;
 
 public class YourQueue {
     private static void solution(List<String> information, int numberOfQueries, int numberOfPatients) {
-        LinkedList<Integer> patients = new LinkedList<>();
-        for (int i = 1; i <= numberOfPatients; i++) {
+        LinkedList<Long> patients = new LinkedList<>();
+        for (long i = 1; i <= numberOfPatients; i++) {
             patients.add(i);
         }
-        List<Integer> orders = new ArrayList<>();
         int query = 0;
         while (query < numberOfQueries) {
             for (String s : information) {
                 if (s.equals("N")) {
-                    Integer firstPatient = patients.remove();
+                    Long firstPatient = patients.remove();
                     patients.add(firstPatient);
-                    orders.add(firstPatient);
+                    System.out.println(firstPatient);
                 }
                 else if (s.charAt(0) == 'E') {
                     String patientToBePrioritizedString = s.substring(2);
-                    Integer patientToBePrioritized = Integer.parseInt(patientToBePrioritizedString);
+                    Long patientToBePrioritized = Long.parseLong(patientToBePrioritizedString);
                     patients.remove(patientToBePrioritized);
                     patients.add(0, patientToBePrioritized);
                 }
                 query++;
             }
         }
-        for (Integer i : orders) {
-            System.out.println(i);
-        }
-
-
     }
 
     public static void main(String[] args) {
