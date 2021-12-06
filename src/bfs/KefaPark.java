071177f9850109f369ea087fe0f896778b401421
minhle0105 <minh.anh.le.cnn@gmail.com>
2021-12-06 15:32:59 -0500

    Consecutive cats

--- src/bfs/KefaPark.java
+++ src/bfs/KefaPark.java
@@ -3,17 +3,11 @@ package bfs;
 import java.util.*;
 
 public class KefaPark {
-    private static List<List<Integer>> graph;
-    private static int V;
-    private static int E;
-    private static List<Integer> path;
-    private static List<Boolean> visited;
 
-    private static int solution (List<List<Integer>> graph, int[] cats) {
-        int result = 0;
+    private static int solution (List<List<Integer>> graph, int[] cats, int catTolerance) {
         Queue<Integer> queue = new LinkedList<>();
         HashMap<Integer, Boolean> hasRestaurant = new HashMap<>();
-        HashMap<Integer, Boolean> hasCat = new HashMap<>();
+        int consecutiveCats = 0;
         for (int i = 1; i < graph.size(); i++) {
             if (graph.get(i).size() == 1) {
                 hasRestaurant.put(i, true);
@@ -24,8 +18,8 @@ public class KefaPark {
         }
 
         int topVer = 1;
-        path = new ArrayList<>();
-        visited = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+        List<Boolean> visited = new ArrayList<>();
         for (int i = 0; i < graph.size(); i++) {
             visited.add(false);
             path.add(-1);
@@ -35,12 +29,24 @@ public class KefaPark {
         queue.add(topVer);
         while (!queue.isEmpty()) {
             int removedVer = queue.remove();
+            if (cats[removedVer - 1] == 1) {
+                consecutiveCats++;
+            }
             for (int i = 0; i < graph.get(removedVer).size(); i++) {
                 int nextVer = graph.get(removedVer).get(i);
+                if (cats[nextVer - 1] == 1) {
+                    if (consecutiveCats < catTolerance) {
+                        consecutiveCats++;
+                    }
+                    else {
+                        continue;
+                    }
+                }
                 if (cats[nextVer - 1] == 0 && !visited.get(nextVer)) {
                     queue.add(nextVer);
                     visited.set(nextVer, true);
                     path.set(nextVer, removedVer);
+                    consecutiveCats = 0;
                 }
             }
         }
@@ -56,28 +62,28 @@ public class KefaPark {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        V = Integer.parseInt(sc.next());
-        E = V - 1;
+        int v = Integer.parseInt(sc.next());
+        int e = v - 1;
         int catTolerance = Integer.parseInt(sc.next());
-        graph = new ArrayList<>();
-        int[] hasCat = new int[V];
+        List<List<Integer>> graph = new ArrayList<>();
+        int[] hasCat = new int[v];
         for (int i = 0; i < hasCat.length; i++) {
             hasCat[i] = Integer.parseInt(sc.next());
         }
 
-        for (int i = 0; i < V + 1; i++) {
+        for (int i = 0; i < v + 1; i++) {
             graph.add(new ArrayList<>());
         }
 
 
 
-        for (int i = 0; i < E; i++) {
+        for (int i = 0; i < e; i++) {
             int v1 = Integer.parseInt(sc.next());
             int v2 = Integer.parseInt(sc.next());
             graph.get(v1).add(v2);
             graph.get(v2).add(v1);
         }
-        System.out.println(solution(graph, hasCat));
+        System.out.println(solution(graph, hasCat, catTolerance));
         sc.close();
     }
 }
