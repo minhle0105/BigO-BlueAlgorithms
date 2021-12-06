277aaf9977543e532c1316324c214de499c6b199
minhle0105 <minh.anh.le.cnn@gmail.com>
2021-12-06 18:02:08 -0500

    Visited all points, but count not working

--- src/bfs/Slink.java
+++ src/bfs/Slink.java
@@ -14,27 +14,32 @@ public class Slink {
         queue.add(startPoint[0]);
         queue.add(startPoint[1]);
         visited[0][0] = 1;
+
+        // bắt đầu đếm số vết loang, nếu ô đầu tiên bị loang thì bắt đầu từ 1
         int count = map[startPoint[0]][startPoint[1]] == 1 ? 1 : 0;
         while (!queue.isEmpty()) {
             int x = queue.remove();
             int y = queue.remove();
-            if (map[x][y] == 0 && count == 0) {
-                count++;
-            }
             for (int direction = 0; direction < 4; direction++) {
                 int nextX = x + dR[direction];
                 int nextY = y + dC[direction];
+
+                // trước hết kiểm tra toạ độ có hợp lệ không
                 if ((nextX >= 0 && nextX < row) && (nextY >= 0 && nextY < column)) {
+                    // nếu hợp lệ và chưa thăm thì thêm vào queue
                     if (visited[nextX][nextY] == 0) {
                         queue.add(nextX);
                         queue.add(nextY);
+
+                        // nếu toạ độ này có vết loang thì tăng count lên
                         if (map[nextX][nextY] == 1) {
                             count++;
-                        } else {
+                        } else { // nếu không có vết count thì chuỗi loang bị đứt, nên ghi lại biến count và reset về 0
                             width.add(count);
                             count = 0;
                         }
                     }
+                    // miễn là inbound thì dù có loang hay không cũng là thăm rồi nên đổi thành thăm rồi
                     visited[nextX][nextY] = 1;
                 }
 
@@ -42,10 +47,6 @@ public class Slink {
         }
         return width;
 }
-//    3 3
-//1 1 1
-//1 1 0
-//1 0 1
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
