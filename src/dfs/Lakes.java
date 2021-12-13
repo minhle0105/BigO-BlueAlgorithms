1fc96792a3ae0b5f2abc9ed98fb244f42708f579
minhle0105 <minh.anh.le.cnn@gmail.com>
2021-12-12 20:49:54 -0500

    Lakes doing

--- src/dfs/Lakes.java
+++ src/dfs/Lakes.java
@@ -6,22 +6,26 @@ public class Lakes {
 
     static int[][] visited;
     static final char land = '*';
+    // lakeInfo là để lưu những hồ trong map và kích thước. Ví dụ có 3 node thì là có 3 hồ, giá trị mỗi node là kích cỡ hồ
     static LinkedList<Integer> lakeInfo;
     static final int[] dR = {-1, 1, 0, 0};
     static final int[] dC = {0, 0, -1, 1};
+    // lakePosition là để lưu kích cỡ hồ và vị trí có thể lấp của hồ đó. Giả sử hồ kích cỡ 2 sẽ lấp được ở [1,6] và [1,7]
     static Map<Integer, List<int[]>> lakePosition;
 
+    // kiểm tra xem bao giờ visit hết map
     private static int[] graphIsFull(int[][] visited) {
         for (int i = 0; i < visited.length; i++) {
             for (int j = 0; j < visited[i].length; j++) {
                 if (visited[i][j] == 0) {
-                    return new int[]{i,j};
+                    return new int[]{i, j};
                 }
             }
         }
-        return new int[]{-1,-1};
+        return new int[]{-1, -1};
     }
 
+    // dfs để tìm hồ và kích cỡ
     private static int findLakeSize(int[] startPoint, int numberOfRows, int numberOfColumns) {
         Stack<Integer> stack = new Stack<>();
         List<int[]> positions = new ArrayList<>();
@@ -29,13 +33,13 @@ public class Lakes {
         stack.push(startPoint[0]);
         positions.add(new int[]{startPoint[0], startPoint[1]});
         int count = 1;
+        // isOnBound sẽ là true nếu như hồ này nằm trên đường bao, và nếu thế thì không tính
         boolean isOnBound = false;
         while (!stack.isEmpty()) {
             int thisX = stack.pop();
             int thisY = stack.pop();
             if (thisX == 0 | thisX == numberOfRows - 1 | thisY == 0 || thisY == numberOfColumns - 1) {
                 isOnBound = true;
-                break;
             }
             for (int direction = 0; direction < 4; direction++) {
                 int nextX = thisX + dR[direction];
@@ -47,18 +51,18 @@ public class Lakes {
                         stack.push(nextY);
                         stack.push(nextX);
                         count++;
-                        positions.add(new int[]{nextX,nextY});
+                        positions.add(new int[]{nextX, nextY});
                     }
                 }
             }
         }
         if (!isOnBound) {
+            // nếu không nằm trên đường bao thì thêm vị trí và kích cỡ hồ vào map
             if (lakePosition.containsKey(count)) {
                 for (int[] position : positions) {
                     lakePosition.get(count).add(position);
                 }
-            }
-            else {
+            } else {
                 lakePosition.put(count, positions);
             }
             return count;
@@ -68,8 +72,11 @@ public class Lakes {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        // khởi tạo biến static
         lakeInfo = new LinkedList<>();
         lakePosition = new HashMap<>();
+
+        // đọc input
         int numberOfRows = Integer.parseInt(sc.next());
         int numberOfColumns = Integer.parseInt(sc.next());
         int finalLakes = Integer.parseInt(sc.next());
@@ -88,12 +95,15 @@ public class Lakes {
             }
         }
 
+        // liên tục kiểm tra cho tới khi map hoàn toàn được visit
         while (true) {
+            // lấy điểm có hồ tiếp theo để kiểm tra
             int[] nextStartPoint = graphIsFull(visited);
             if (nextStartPoint[0] == -1) {
                 break;
             }
             visited[nextStartPoint[0]][nextStartPoint[1]] = 1;
+            // nếu lakeSize trả về là 0, tức là nằm trên đường bao, không tính. nếu không thì thêm vào linkedlist
             int lakeSize = findLakeSize(nextStartPoint, numberOfRows, numberOfColumns);
             if (lakeSize == 0) {
                 continue;
@@ -101,12 +111,17 @@ public class Lakes {
             lakeInfo.add(lakeSize);
         }
         int count = 0;
+        // vì cần lấp ít nhất nên sort để lấp từ nhỏ tới lớn
+        // dùng linkedlist là vì liên tục cần dỡ phần tử đầu tiên, sẽ efficient hơn arraylist
         Collections.sort(lakeInfo);
         while (lakeInfo.size() != finalLakes) {
+            // liên tục gỡ cho tới khi bằng số hồ cần sót lại
             int lakeSizeToFill = lakeInfo.removeFirst();
             count += lakeSizeToFill;
+            // lấy toạ độ hồ cần lấp
             List<int[]> pointsToFill = lakePosition.get(lakeSizeToFill);
             for (int[] pointToFill : pointsToFill) {
+                // lấp bằng land
                 map[pointToFill[0]][pointToFill[1]] = land;
             }
         }
