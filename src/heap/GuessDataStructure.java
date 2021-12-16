52d09a0cb7654401e5f0d7170073cee79e9f4f5c
minhle0105 <minh.anh.le.cnn@gmail.com>
2021-12-16 04:55:10 -0500

    Phai xu ly song song

--- src/heap/GuessDataStructure.java
+++ src/heap/GuessDataStructure.java
@@ -9,7 +9,6 @@ public class GuessDataStructure {
     static PriorityQueue<Integer> heap;
 
     private static String solution (List<List<Integer>> queryDetails) {
-        String result = "";
         String stackToString = "";
         String queueToString = "";
         String heapToString = "";
@@ -32,6 +31,12 @@ public class GuessDataStructure {
             }
         }
 
+        if (queryToString.length() < stackToString.length()) {
+            stackToString = stackToString.substring(0, queryToString.length());
+            queueToString = queueToString.substring(0, queryToString.length());
+            heapToString = heapToString.substring(0, queryToString.length());
+        }
+
         if (queryToString.equals(stackToString)) {
             if (queryToString.equals(queueToString) | queryToString.equals(heapToString)) {
                 return "not sure";
@@ -41,7 +46,16 @@ public class GuessDataStructure {
             }
         }
 
-        else if (queryToString.equals(queueToString)) {
+        if (queryToString.equals(heapToString)) {
+            if (queryToString.equals(stackToString) | queryToString.equals(queueToString)) {
+                return "not sure";
+            }
+            else {
+                return "priority queue";
+            }
+        }
+
+        if (queryToString.equals(queueToString)) {
             if (queryToString.equals(stackToString) | queryToString.equals(heapToString)) {
                 return "not sure";
             }
@@ -50,39 +64,38 @@ public class GuessDataStructure {
             }
         }
 
-        else if (queryToString.equals(heapToString)) {
-            if (queryToString.equals(queueToString) | queryToString.equals(stackToString)) {
-                return "not sure";
-            }
-            else {
-                return "priority queue";
-            }
-        }
         return "impossible";
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int numberOfQueries = Integer.parseInt(sc.next());
-        List<List<Integer>> queryDetails = new ArrayList<>();
-        List<Integer> query;
-        queue = new LinkedList<>();
-        stack = new Stack<>();
-        heap = new PriorityQueue<>((o1, o2) -> o2.compareTo(o1));
-        for (int i = 0; i < numberOfQueries; i++) {
-            query = new ArrayList<>();
-            int q1 = Integer.parseInt(sc.next());
-            int q2 = Integer.parseInt(sc.next());
-            query.add(q1);
-            query.add(q2);
-            if (q1 == 1) {
-                queue.add(q2);
-                stack.add(q2);
-                heap.add(q2);
+        List<String> output = new ArrayList<>();
+        while (sc.hasNextInt()) {
+            int numberOfQueries = Integer.parseInt(sc.next());
+            List<List<Integer>> queryDetails = new ArrayList<>();
+            List<Integer> query;
+            queue = new LinkedList<>();
+            stack = new Stack<>();
+            heap = new PriorityQueue<>((o1, o2) -> o2.compareTo(o1));
+            for (int i = 0; i < numberOfQueries; i++) {
+                query = new ArrayList<>();
+                int q1 = Integer.parseInt(sc.next());
+                int q2 = Integer.parseInt(sc.next());
+                query.add(q1);
+                query.add(q2);
+                if (q1 == 1) {
+                    queue.add(q2);
+                    stack.add(q2);
+                    heap.add(q2);
+                }
+                queryDetails.add(query);
             }
-            queryDetails.add(query);
+            output.add(solution(queryDetails));
         }
-        System.out.println(solution(queryDetails));
+
+        for (String s : output) {
+            System.out.println(s);
+        }
         sc.close();
     }
 
