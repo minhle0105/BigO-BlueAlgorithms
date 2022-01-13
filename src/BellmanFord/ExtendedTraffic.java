0e6ec9d408521766cab25da1750502e1ea12729d
minhle0105 <minh.anh.le.cnn@gmail.com>
2022-01-13 16:20:52 -0500

    Test dev branch

--- src/BellmanFord/ExtendedTraffic.java
+++ src/BellmanFord/ExtendedTraffic.java
@@ -1,4 +1,10 @@
 package BellmanFord;
 
+import java.util.Scanner;
+
 public class ExtendedTraffic {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.close();
+    }
 }
