5e389e14c3631fddfd40ee9612d97c027b5fa3da
minhle0105 <minh.anh.le.cnn@gmail.com>
2022-01-13 16:24:12 -0500

    Test dev local

--- src/BellmanFord/ExtendedTraffic.java
+++ src/BellmanFord/ExtendedTraffic.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class ExtendedTraffic {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int test = Integer.parseInt(sc.next());
         sc.close();
     }
 }
