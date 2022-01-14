fa62bb1c4d478f9a493fdd3ddbd0c80c24d2f0b2
minhle0105 <minh.anh.le.cnn@gmail.com>
2022-01-14 13:00:51 -0500

    Doing Chicago

---
+++ src/BellmanFord/Chicago.java
@@ -0,0 +1,78 @@
+package BellmanFord;
+
+import java.util.*;
+
+class CityEdge {
+    int source;
+    int target;
+    double weight;
+
+    public CityEdge(int source, int target, double weight) {
+        this.source = source;
+        this.target = target;
+        this.weight = weight;
+    }
+}
+
+public class Chicago {
+
+    private static boolean bellmanFord(CityEdge[] graph, int src, double[] distance, int[] path, int numberOfVertice) {
+        distance[src] = 1;
+        for (int i = 1; i <= numberOfVertice - 1; i++) {
+            for (CityEdge edge : graph) {
+                int u = edge.source;
+                int v = edge.target;
+                double w = edge.weight;
+                if (distance[u] != Integer.MAX_VALUE && distance[u] * w < distance[v]) {
+                    distance[v] = distance[u] * w;
+                    path[v] = u;
+                }
+            }
+        }
+        for (CityEdge edge : graph) {
+            int u = edge.source;
+            int v = edge.target;
+            double w = edge.weight;
+            if (distance[u] != Integer.MAX_VALUE && distance[u] * w < distance[v]) {
+                return false;
+            }
+        }
+        return true;
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            int n = Integer.parseInt(sc.next());
+            if (n == 0) {
+                break;
+            }
+            int m = Integer.parseInt(sc.next());
+
+            CityEdge[] graph = new CityEdge[m];
+            double[] distance = new double[n+1];
+            int[] path = new int[n+1];
+            Arrays.fill(distance, Double.MAX_VALUE);
+            Arrays.fill(path, -1);
+            for (int i = 0; i < m; i++) {
+                int src = Integer.parseInt(sc.next());
+                int dst = Integer.parseInt(sc.next());
+                double weight = Double.parseDouble(sc.next());
+                CityEdge edge = new CityEdge(src, dst, weight/100);
+                graph[i] = edge;
+            }
+            boolean result = bellmanFord(graph, 1, distance, path, n);
+            System.out.println(result);
+            for (int i = 1; i < distance.length; i++) {
+                distance[i] *= 100;
+            }
+            System.out.println(distance[n]);
+
+
+        }
+
+
+        sc.close();
+    }
+}
--- src/BellmanFord/ShortestPathNegWeight.java
+++ src/BellmanFord/ShortestPathNegWeight.java
@@ -1,16 +1,101 @@
 package BellmanFord;
 
-import java.util.Scanner;
+import java.util.*;
+
+class ShortestPathEdge {
+    int source;
+    int target;
+    int weight;
+
+    public ShortestPathEdge(int source, int target, int weight) {
+        this.source = source;
+        this.target = target;
+        this.weight = weight;
+    }
+}
 
 public class ShortestPathNegWeight {
+
+    public static boolean bellmanFord(ShortestPathEdge[] graph, int src, int[] distance, int[] path, int numberOfVertice, Set<Integer> possibleNegative) {
+        distance[src] = 0;
+        for (int i = 1; i <= numberOfVertice - 1; i++) {
+            for (ShortestPathEdge edge : graph) {
+                int u = edge.source;
+                int v = edge.target;
+                int w = edge.weight;
+                if (distance[u] != Integer.MAX_VALUE && distance[u] + w < distance[v]) {
+                    distance[v] = distance[u] + w;
+                    path[v] = u;
+                }
+            }
+        }
+        for (int i = 1; i <= numberOfVertice - 1; i++) {
+            for (ShortestPathEdge edge : graph) {
+                int u = edge.source;
+                int v = edge.target;
+                int w = edge.weight;
+                if (distance[u] != Integer.MAX_VALUE && distance[u] + w < distance[v]) {
+                    possibleNegative.add(u);
+                }
+            }
+        }
+
+        return !(possibleNegative.size() > 0);
+
+    }
+
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = Integer.parseInt(sc.next());
-        int m = Integer.parseInt(sc.next());
-        int q = Integer.parseInt(sc.next());
-        int s = Integer.parseInt(sc.next());
-        for (int i = 0; i < m; i++) {
+        List<List<String>> results = new ArrayList<>();
+        while (true) {
+            int n = Integer.parseInt(sc.next());
+            int m = Integer.parseInt(sc.next());
+            int q = Integer.parseInt(sc.next());
+            int s = Integer.parseInt(sc.next());
+            if (n == 0) {
+                break;
+            }
+            List<String> thisResult = new ArrayList<>();
+            ShortestPathEdge[] graph = new ShortestPathEdge[m];
+            int[] distance = new int[n];
+            int[] path = new int[n];
+            Arrays.fill(path, -1);
+            Arrays.fill(distance, Integer.MAX_VALUE);
+            for (int i = 0; i < m; i++) {
+                int src = Integer.parseInt(sc.next());
+                int dst = Integer.parseInt(sc.next());
+                int weight = Integer.parseInt(sc.next());
+                ShortestPathEdge edge = new ShortestPathEdge(src, dst, weight);
+                graph[i] = edge;
+            }
 
+            Set<Integer> possibleNegative = new HashSet<>();
+
+            bellmanFord(graph, s, distance, path, n, possibleNegative);
+
+            for (int i = 0; i < q; i++) {
+                int dst = Integer.parseInt(sc.next());
+                if (possibleNegative.contains(dst)) {
+                    thisResult.add("-Infinity");
+                }
+                else {
+                    if (distance[dst] == Integer.MAX_VALUE) {
+                        thisResult.add("Impossible");
+                    }
+                    else {
+                        thisResult.add(distance[dst] + "");
+                    }
+                }
+            }
+            results.add(thisResult);
+        }
+
+        for (List<String> result : results) {
+            for (String r : result) {
+                System.out.println(r);
+            }
+            System.out.println();
         }
         sc.close();
     }
