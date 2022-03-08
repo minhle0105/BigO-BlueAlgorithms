dfe31b95ab944047c6adabbcf4986997966f9cde
minhle0105 <minh.anh.le.cnn@gmail.com>
2022-03-08 13:20:30 -0500

    MUH Done

--- src/finalExam/MUH.java
+++ src/finalExam/MUH.java
@@ -8,36 +8,48 @@ public class MUH {
         Scanner sc = new Scanner(System.in);
         int n = Integer.parseInt(sc.next());
         int[] tasks = new int[n];
+
         for (int i = 0; i < n; i++) {
             tasks[i] = Integer.parseInt(sc.next());
         }
+
+
         Map<Integer, List<Integer>> map = new HashMap<>();
+
         for (int i = 0; i < n; i++) {
             if (!map.containsKey(tasks[i])) {
                 map.put(tasks[i], new ArrayList<>());
             }
             map.get(tasks[i]).add(i);
         }
+
         Arrays.sort(tasks);
+
         long ways = 1;
+
         for (Integer key : map.keySet()) {
-            ways *= map.get(key).size();
+            ways = ways * map.get(key).size();
+            if(ways >=3l) break;
         }
-        if (ways >= 3) {
+
+        if (ways >= 3l) {
             System.out.println("YES");
             List<List<Integer>> options = new ArrayList<>();
             options.add(new ArrayList<>());
             options.add(new ArrayList<>());
             options.add(new ArrayList<>());
+
             int i = 0;
             for (Integer key : map.keySet()) {
                 for (Integer index : map.get(key)) {
                     options.get(i).add((index + 1));
                 }
             }
+
             i += 1;
             boolean swap = false;
             int hasSeenKey = -1;
+
             for (Integer key : map.keySet()) {
                 List<Integer> indices = map.get(key);
                 if (indices.size() >= 2 && !swap) {
@@ -57,14 +69,23 @@ public class MUH {
             }
 
             i += 1;
-            if (map.get(hasSeenKey).size() == 3) {
-                options.get(i).add(map.get(hasSeenKey).get(0) + 1);
-                options.get(i).add(map.get(hasSeenKey).get(2) + 1);
-                options.get(i).add(map.get(hasSeenKey).get(1) + 1);
+            if (map.get(hasSeenKey).size() >= 3) {
+                // Xử lí trường hợp tất cả các số khác xuất hiện 1
+                // lần và chỉ có một số xuất hiện 3 lần trở lên
                 for (Integer key : map.keySet()) {
                     if (key != hasSeenKey) {
                         for (Integer index : map.get(key)) {
                             options.get(i).add(index + 1);
+                        }
+                    }
+                    else {
+                        // Chuyển phần xử lý hasSeenKey vào đây để các key được
+                        // thêm vào theo đúng thứ tự (nhỏ đến lớn)
+                        options.get(i).add(map.get(hasSeenKey).get(0) + 1);
+                        options.get(i).add(map.get(hasSeenKey).get(2) + 1);
+                        options.get(i).add(map.get(hasSeenKey).get(1) + 1);
+                        for(int j=3; j<map.get(hasSeenKey).size(); j++) {
+                            options.get(i).add(map.get(hasSeenKey).get(j));
                         }
                     }
                 }
