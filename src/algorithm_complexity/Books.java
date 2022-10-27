cae14314fa5d41f17c9978299b6295ee1588fe8d
minhle0105 <minh.anh.le.cnn@gmail.com>
2022-10-27 17:37:46 -0400

    Added comment for Books

--- src/algorithm_complexity/Books.java
+++ src/algorithm_complexity/Books.java
@@ -1,8 +1,5 @@
 package algorithm_complexity;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -10,31 +7,52 @@ import java.util.Scanner;
 
 public class Books {
 
-    private static int solution(int[] list, int time) {
+    private static int solution(int[] list, int timeLimit) {
+        // numberOfBooksCanBeReadFromHere stores the data to tell you
+        // if you start from the i-th book, then numberOfBooksCanBeReadFromHere[i] is the
+        // number of books you can finish
+        // --> the max value of numberOfBooksCanBeReadFromHere will be the answer
         List<Integer> numberOfBooksCanBeReadFromHere = new ArrayList<>();
-        int firstPointer = 0;
-        int secondPointer = 0;
-        int sumOfSub = list[firstPointer];
+
+        int firstPointer = 0; // the first book we start on
+        int secondPointer = 0; // the last book we can read (trying to extend from firstPointer)
+        // timeConsumedSoFar starts counting from reading book 1
+        int timeConsumedSoFar = list[firstPointer];
+        // while loop
         while (firstPointer < list.length) {
-            if (sumOfSub <= time && secondPointer < list.length - 1) {
+            // if we still have time && if we haven't reached the last book
+            if (timeConsumedSoFar <= timeLimit && secondPointer < list.length - 1) {
+                // extend secondPointer
                 secondPointer++;
-                sumOfSub += list[secondPointer];
+                // we can read this book --> add the time to timeConsumedSoFar
+                timeConsumedSoFar += list[secondPointer];
             }
+            // else (we either run out of time, or we have reached the last book
             else {
-                if (secondPointer < list.length - 1 || sumOfSub >= time) {
+                // have not reached the last book or running out of time
+                if (secondPointer < list.length - 1 || timeConsumedSoFar > timeLimit) {
+                    // timeConsumedSoFar is already > time limit, which means we cannot read
+                    // this current book at second pointer
+                    // --> we have to subtract one from second pointer when adding to result
                     numberOfBooksCanBeReadFromHere.add((secondPointer - 1) - firstPointer + 1);
                 }
                 else {
+                    // reached the last book
                     numberOfBooksCanBeReadFromHere.add((secondPointer) - firstPointer + 1);
                 }
+                // if the book we start is actually the last book already --> break
                 if (firstPointer == list.length - 1) {
                     break;
                 }
-                sumOfSub = list[firstPointer+1];
+                // otherwise, we try start from the next book --> set timeConsumedSoFar to be the next book
+                timeConsumedSoFar = list[firstPointer + 1];
+                // moving to the next book
                 firstPointer++;
+                // start extending from the next book
                 secondPointer = firstPointer;
             }
         }
+        // max here is the max number of books that can be read
         int maxNumberOfBooks = numberOfBooksCanBeReadFromHere.get(0);
         for (Integer i : numberOfBooksCanBeReadFromHere) {
             if (i > maxNumberOfBooks) {
@@ -57,24 +75,4 @@ public class Books {
         System.out.println(solution(list, totalTimeAvailable));
         sc.close();
     }
-
-    @Test
-    void firstTest() {
-        int[] list = {3,1,2,1};
-        int time = 5;
-        int expected = 3;
-        int actual = solution(list, time);
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    void secondTest() {
-        int[] list = {2,2,3};
-        int time = 3;
-        int expected = 1;
-        int actual = solution(list, time);
-        Assertions.assertEquals(expected, actual);
-    }
-
-
 }
