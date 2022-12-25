fc35de85fa25ccad6e261ed196e8a6b79f1df9df
minhle0105 <minh.anh.le.cnn@gmail.com>
2022-12-25 07:51:45 +0700

    Commenting Lesson 2 / Constant Range

--- src/algorithm_complexity/ConstantRange.java
+++ src/algorithm_complexity/ConstantRange.java
@@ -6,6 +6,8 @@ public class ConstantRange {
 
     private static int solution (List<Integer> list) {
         int maxCanGo = 1;
+
+        // 2 cái pointer này là để đánh dấu các đoạn gần như không đổi
         int firstPointer = 0;
         int secondPointer = 1;
 
@@ -21,6 +23,7 @@ public class ConstantRange {
             while(secondPointer < list.size()) {
 
                 //Cố để thêm phần tử hiện tại
+                // Theo kĩ thuật đếm frequency của phần tử
                 if(hashSet.containsKey(list.get(secondPointer))) {
                     hashSet.put(list.get(secondPointer), hashSet.get(list.get(secondPointer)) + 1);
                 }
@@ -28,8 +31,11 @@ public class ConstantRange {
                     hashSet.put(list.get(secondPointer), 1);
                 }
 
-                //Nếu thêm vô rồi mà vượt quá 2 phần tử thì bỏ nó
-                if(hashSet.size()==3) {
+                //Nếu thêm vô rồi mà vượt quá 2 phần tử thì bỏ nó và cũng không đi tiếp while loop
+                // là vì đoạn dữ liệu cho không có bước nhảy lớn giữa các phần tử
+                // mà nếu 1 đoạn có quá 2 phần tử thì độ chênh giữa max và min của đoạn
+                // chắc chắn sẽ lớn hơn 1 --> không hợp lệ
+                if(hashSet.size() == 3) {
                     hashSet.remove(list.get(secondPointer));
                     break;
                 }
@@ -37,14 +43,17 @@ public class ConstantRange {
                 // Còn không thì cộng lên để vòng lặp sau thêm
                 secondPointer++;
             }
+            // cần cập nhật lại cái maxCanGo sau mỗi lần đẩy secondPointer lên xa nhất có thể
             if(secondPointer - firstPointer > maxCanGo) {
                 maxCanGo = secondPointer - firstPointer;
-//        		System.out.println(firstPointer + " " + secondPointer);
             }
 
+            // đẩy lên đoạn tiếp để tìm xem có đoạn nào dài hơn không,
+            // thì nếu ví dụ đoạn là 4 5 5 5 (firstPointer ở 0) --> giờ chỉ xét đoạn 5 5 5
             if(hashSet.get(list.get(firstPointer)) == 1) {
                 hashSet.remove(list.get(firstPointer));
             }
+            // còn nếu là 4 4 5 5 5 (firstPointer ở 0) --> giờ xét đoạn 4 5 5 5
             else {
                 hashSet.put(list.get(firstPointer), hashSet.get(list.get(firstPointer)) - 1);
             }
