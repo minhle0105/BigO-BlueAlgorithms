8d43833b0d642715fc8f5585a3e917719e5e59e7
minhle0105 <minh.anh.le.cnn@gmail.com>
2022-12-30 12:59:24 -0500

    Commenting Eight Point Sets

--- src/sorting/EightPointSets.java
+++ src/sorting/EightPointSets.java
@@ -19,16 +19,22 @@ public class EightPointSets {
             }
         }
 
+        // nếu không có đủ 3 toạ độ x hoặc 3 toạ độ y thì chắc chắn ko generate được
+        // đúng số points cần thiết --> ugly
         if (Xs.size() != 3 | Ys.size() != 3) {
             return "ugly";
         }
 
+        // sort để đảm bảo thứ tự
         Collections.sort(Xs);
         Collections.sort(Ys);
 
+        // tạo ra 3x3 = 9 points từ 3 toạ độ x và 3 toạ độ y
         List<List<Integer>> generatedPoints = new ArrayList<>();
         for (Integer x : Xs) {
             for (Integer y : Ys) {
+                // vì bỏ qua trung bình, mà đã sort rồi nên điểm trung bình
+                // chắc chắn là index 1
                 if (Xs.indexOf(x) == 1 && Ys.indexOf(y) == 1) {
                     continue;
                 }
@@ -41,6 +47,10 @@ public class EightPointSets {
             }
         }
 
+        // vì list các điểm được tạo ra đã được sắp xếp theo x và y
+        // (ở trên Collections.sort(x) và Collections.sort(y)
+        // nên ở đây viết comparator sort lại list points đã cho
+        // để so sánh 2 list
         Collections.sort(points, (o1, o2) -> {
             if (!o1.get(0).equals(o2.get(0))) {
                 return o1.get(0).compareTo(o2.get(0));
@@ -49,6 +59,8 @@ public class EightPointSets {
         });
 
         for (int i = 0; i < points.size(); i++) {
+            // nếu có điểm nào trong list đã cho khác với điểm tại index tương ứng
+            // trong số các điểm được generated --> ugly
             if (!points.get(i).get(0).equals(generatedPoints.get(i).get(0)) | !points.get(i).get(1).equals(generatedPoints.get(i).get(1))) {
                 return "ugly";
             }
